31b3f751ac5ba80bd26cd163f03542da23166eec
jinoosss <jwchoi@onbloc.xyz>
2024-09-09 14:44:27 +0900

    chore: add comments to sdk modules

--- packages/sdk/src/core/connection/connection-state.ts
+++ packages/sdk/src/core/connection/connection-state.ts
@@ -12,15 +12,27 @@ export class ConnectionStateManager {
 
   private state: ConnectionState = ConnectionState.DISCONNECTED;
 
+  /**
+   * Retrieves the current connection state.
+   * @returns The current connection state.
+   */
   getState(): ConnectionState {
     return this.state;
   }
 
+  /**
+   * Sets a new connection state and saves it to storage.
+   * @param state - The new connection state to set.
+   */
   setState(state: ConnectionState): void {
     this.state = state;
     this.saveState();
   }
 
+  /**
+   * Loads the connection state from storage and updates the internal state.
+   * If the stored state is 'CONNECTED', it sets the state to CONNECTED.
+   */
   loadState(): void {
     const savedState = getSessionStorageItem(ConnectionStateManager.STORAGE_KEY);
     if (savedState === ConnectionState.CONNECTED.toString()) {
@@ -28,6 +40,10 @@ export class ConnectionStateManager {
     }
   }
 
+  /**
+   * Saves the current connection state to storage.
+   * The state is saved under the key defined by STORAGE_KEY.
+   */
   private saveState(): void {
     setSessionStorageItem(ConnectionStateManager.STORAGE_KEY, this.state.toString());
   }
