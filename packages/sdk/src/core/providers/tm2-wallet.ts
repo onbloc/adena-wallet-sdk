89a80c538483428e28b48a470dc0ccc8e8f49998
jinoosss <jwchoi@onbloc.xyz>
2024-09-09 15:04:06 +0900

    feat: add getWallet method in wallet provider

--- packages/sdk/src/core/providers/tm2-wallet.ts
+++ packages/sdk/src/core/providers/tm2-wallet.ts
@@ -1,7 +1,10 @@
+import { Wallet as TM2Wallet } from '@gnolang/tm2-js-client';
 import { WalletProvider } from './wallet';
 
 export interface TM2WalletProvider extends WalletProvider {
   connect(): Promise<boolean>;
 
   disconnect(): Promise<boolean>;
+
+  getWallet(): TM2Wallet | null;
 }
