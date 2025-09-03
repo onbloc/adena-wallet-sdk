bbe8b1ea52ac1556d9d6df43b39979f56f203aa3
jinoosss <112360739+jinoosss@users.noreply.github.com>
2025-09-03 14:56:32 +0900

    feat: export types

--- packages/sdk/src/core/types/index.ts
+++ packages/sdk/src/core/types/index.ts
@@ -1,5 +1,6 @@
 export * from './account.types';
 export * from './config.types';
 export * from './network.types';
+export * from './proto';
 export * from './transaction.types';
 export * from './wallet.types';
