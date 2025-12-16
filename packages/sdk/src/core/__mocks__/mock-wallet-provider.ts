437772b0237518c23c297f1c4f53ff9744d41c1b
yjin <devyzyn@gmail.com>
2025-12-16 19:03:08 +0900

    test: mock-wallet-provider

--- packages/sdk/src/core/__mocks__/mock-wallet-provider.ts
+++ packages/sdk/src/core/__mocks__/mock-wallet-provider.ts
@@ -22,18 +22,18 @@ export const mockWalletProvider: jest.Mocked<WalletProvider> = {
   addNetwork: jest.fn().mockResolvedValue(makeResponseMessage(WalletResponseSuccessType.ADD_NETWORK_SUCCESS)),
   signTransaction: jest.fn().mockResolvedValue(makeResponseMessage(WalletResponseSuccessType.SIGN_SUCCESS)),
   broadcastTransaction: jest.fn().mockResolvedValue(makeResponseMessage(WalletResponseSuccessType.TRANSACTION_SUCCESS)),
-  createMultisigAccount: jest.fn().mockImplementation(() => {
-    return;
-  }),
-  createMultisigTransaction: jest.fn().mockImplementation(() => {
-    return;
-  }),
-  signMultisigTransaction: jest.fn().mockImplementation(() => {
-    return;
-  }),
-  broadcastMultisigTransaction: jest.fn().mockImplementation(() => {
-    return;
-  }),
+  createMultisigAccount: jest
+    .fn()
+    .mockResolvedValue(makeResponseMessage(WalletResponseSuccessType.CREATE_MULTISIG_ACCOUNT_SUCCESS)),
+  createMultisigTransaction: jest
+    .fn()
+    .mockResolvedValue(makeResponseMessage(WalletResponseSuccessType.CREATE_MULTISIG_TRANSACTION_SUCCESS)),
+  signMultisigTransaction: jest
+    .fn()
+    .mockResolvedValue(makeResponseMessage(WalletResponseSuccessType.SIGN_MULTISIG_TRANSACTION_SUCCESS)),
+  broadcastMultisigTransaction: jest
+    .fn()
+    .mockResolvedValue(makeResponseMessage(WalletResponseSuccessType.BROADCAST_MULTISIG_TRANSACTION_SUCCESS)),
   onChangeAccount: jest.fn().mockImplementation(() => {
     return;
   }),
